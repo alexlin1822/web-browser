45218bb6f53d18edf3142ccff4c3367a42922fa2
Alex <alexlin1822@hotmail.com>
2023-08-06 18:07:03 -0400

    Added Vroswer components

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import Home from "./pages/Home";
 
 export default function App() {
   // return <Browser />;
-  //return <Signup />;
+  // return <Signup />;
   // return <Login />;
   return <Home />;
 }
